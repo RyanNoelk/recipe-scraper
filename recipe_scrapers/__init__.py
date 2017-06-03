f41e7b1a2087222baf455de567839642dfb463ce
ryan.noelk <ryannoelk@gmail.com>
2017-06-03 17:35:54 -0400

    dropping more dead code

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -4,40 +4,14 @@ import re
 import datetime
 
 from .allrecipes import AllRecipes
-from .bonappetit import BonAppetit
 from .budgetbytes import BudgetBytes
 from .budgetbytesv2 import BudgetBytesv2
-from .cookstr import Cookstr
-from .epicurious import Epicurious
-from .finedininglovers import FineDiningLovers
-from .foodrepublic import FoodRepublic
-from .jamieoliver import JamieOliver
-from .mybakingaddiction import MyBakingAddiction
-from .simplyrecipes import SimplyRecipes
-from .steamykitchen import SteamyKitchen
-from .tastykitchen import TastyKitchen
-from .thevintagemixer import TheVintageMixer
-from .twopeasandtheirpod import TwoPeasAndTheirPod
-from .whatsgabycooking import WhatsGabyCooking
 
 
 SCRAPERS = {
     AllRecipes.host(): AllRecipes,
-    # BonAppetit.host(): BonAppetit,
     BudgetBytes.host(): BudgetBytes,
     BudgetBytesv2.host(): BudgetBytesv2,
-    # Cookstr.host(): Cookstr,
-    # Epicurious.host(): Epicurious,
-    # FineDiningLovers.host(): FineDiningLovers,
-    # FoodRepublic.host(): FoodRepublic,
-    # JamieOliver.host(): JamieOliver,
-    # MyBakingAddiction.host(): MyBakingAddiction,
-    # SimplyRecipes.host(): SimplyRecipes,
-    # SteamyKitchen.host(): SteamyKitchen,
-    # TastyKitchen.host(): TastyKitchen,
-    # TheVintageMixer.host(): TheVintageMixer,
-    # TwoPeasAndTheirPod.host(): TwoPeasAndTheirPod,
-    # WhatsGabyCooking.host(): WhatsGabyCooking,
 }
 
 
